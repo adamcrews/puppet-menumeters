4eadfb005c96ca24ebfd0dd5c4d01203784d1409
Adam Crews <adam@puppetlabs.com>
2013-11-30 09:53:11 -0800

    Initial Commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,31 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# == Class: menumeters
+#
+# Install the menumeters package
+#
+# === Parameters
+#
+# None.
+#
+# === Variables
+#
+# None.
+#
+# === Examples
+#
+# include menumeters
+#
+# === Authors
+#
+# Adam Crews <adam@puppetlabs.com>
+#
+# === Copyright
+#
+# Copyright 2013 Adam Crews, unless otherwise nodted.
+#
+class menumeters {
+  package { 'menumeters':
+    ensure   => installed,
+    source   => 'http://www.ragingmenace.com/software/download/MenuMeters.dmg',
+    provider => 'appdmg',
+  }
 }
