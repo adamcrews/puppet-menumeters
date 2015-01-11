7c828459435b6cd179cd4823f6a49465c2aed0b5
David Warden <dfwarden@gmail.com>
2015-01-10 22:05:22 -0500

    first attempt at parameterizing and documenting menumeters class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,26 @@
 # == Class: menumeters
 #
-# Install the menumeters package
+# Install menumeters, by default to the current user's Library folder.
 #
 # === Parameters
 #
-# None.
+# [*installer_source*]
+#   String of URL to MenuMeters.dmg.
+#
+#   default: http://www.ragingmenace.com/software/download/MenuMeters.dmg
+#
+# [*installer_tool*]
+#   String of full path to MenuMeters' install script.
+#
+#   default: /Applications/MenuMeters Installer.app/Contents/Resources/InstallTool
+#
+# [*installer_flag*]
+#   String of arguments to $installer_tool. Known values are
+#   --installuser and --installlibrary, which are mutually
+#   exclusive. Elevated privs are likely required for
+#   --installlibrary, which has not been tested.
+#
+#   default: --installuser
 #
 # === Variables
 #
@@ -17,21 +33,34 @@
 # === Authors
 #
 # Adam Crews <adam@puppetlabs.com>
+# David Warden <dfwarden@gmail.com>
 #
 # === Copyright
 #
 # Copyright 2013 Adam Crews, unless otherwise nodted.
 #
-class menumeters {
-  package { 'menumeters':
-    ensure   => installed,
-    source   => 'http://www.ragingmenace.com/software/download/MenuMeters.dmg',
-    provider => 'appdmg',
-    before   => Exec['menumeters_user_library_install'],
+class menumeters (
+  $installer_source = 'http://www.ragingmenace.com/software/download/MenuMeters.dmg',
+  $installer_tool = '/Applications/MenuMeters Installer.app/Contents/Resources/InstallTool',
+  $installer_flag = '--installuser',
+) {
+
+  $installer_creates = $installer_flag ? {
+    '--installuser' => "/Users/${::boxen_user}/Library/PreferencePanes/MenuMeters.prefPane",
+    '--installlibrary' => '/Library/PreferencePanes/MenuMeters.prefPane',
+    default => fail("Value for \$installer_tool must be --installuser or --installlibrary, not ${installer_tool}."),
   }
 
-  exec { 'menumeters_user_library_install':
-    command  => '"/Applications/MenuMeters Installer.app/Contents/Resources/InstallTool" --installuser',
-    creates  => "/Users/${::boxen_user}/Library/PreferencePanes/MenuMeters.prefPane",
+  package { 'menumeters':
+    ensure   => installed,
+    source   => $installer_source,
+    provider => 'appdmg',
+    before   => Exec['exec_menumeters_prefpane_install'],
   }
+
+  exec { 'exec_menumeters_prefpane_install':
+    command  => "\"${installer_tool}\" ${installer_flag}",
+    creates  => $installer_creates,
+  }
+
 }
