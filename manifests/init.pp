637e5b4baedee91110b1e91d569c72deb883164d
David Warden <dfwarden@gmail.com>
2015-01-10 19:52:54 -0500

    added exec resource to install menumeters to users library folder if it doesn't already exist

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,5 +27,11 @@ class menumeters {
     ensure   => installed,
     source   => 'http://www.ragingmenace.com/software/download/MenuMeters.dmg',
     provider => 'appdmg',
+    before   => Exec['menumeters_user_library_install'],
+  }
+
+  exec { 'menumeters_user_library_install':
+    command  => '"/Applications/MenuMeters Installer.app/Contents/Resources/InstallTool" --installuser',
+    creates  => "/Users/${::boxen_user}/Library/PreferencePanes/MenuMeters.prefPane",
   }
 }
